afbc9522c6d123ef7216d42151e30182c06a659c
elecpiano <elecpiano@gmail.com>
2014-04-24 14:54:45 +0800

    change rotation logic to fix rotation arrangement bug

--- UnityGrass/Assets/GrassBehavior.cs
+++ UnityGrass/Assets/GrassBehavior.cs
@@ -5,12 +5,14 @@ public class GrassBehavior : MonoBehaviour
 {
 
     Transform[] joints = new Transform[10];
+    float previousAngle = 0f;
 
     void Start()
     {
         for (int i = 0; i < 10; i++)
         {
             joints[i] = transform.Find("Joint_" + i.ToString());
+            previousAngle = 0f;
         }
     }
 
@@ -20,10 +22,14 @@ public class GrassBehavior : MonoBehaviour
 
     public void Bend(float angle)
     {
+        float deltaAngle = angle - previousAngle;
         for (int i = 9; i >= 0; i--)
         {
-            joints[i].eulerAngles = new Vector3(0f, 0f, angle);
-            angle -= 0.1f * angle;
+            //joints[i].eulerAngles = new Vector3(0f, 0f, angle);
+            //angle -= 0.1f * angle;
+            joints[i].Rotate(0f, 0f, deltaAngle);
+            deltaAngle -= 0.1f * deltaAngle;
         }
+        previousAngle = angle;
     }
 }
